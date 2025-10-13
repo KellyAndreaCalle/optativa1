842c3c625cfbcb7ad0aa9e7120567f80b90f47d5
Kelly Andrea Calle <kellycalle317298@correo.itm.edu.co>
2025-10-13 16:38:58 -0500

    Proyecto de codicionales y operadores creado

---
+++ conditionals_and_operators/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Conditionals*/
+//Console.WriteLine("Ingrese un número");
+//int num = int.Parse(Console.ReadLine());
+//if (num > 0) Console.WriteLine("El número es positivo");
+//else if (num < 0) Console.WriteLine("El número es negativo");
+//else Console.WriteLine("El número es cero");
+/* end conditionals*/
+
+/*Operators*/
+Console.WriteLine("Ingrese el primer número");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo número");
+double b = double.Parse(Console.ReadLine());
+const string indeterminado = "indeterminado";
+Console.WriteLine($"Suma: {a + b}, Resta: {a - b}, Multiplicación: {a * b}, División: {(b!=0 ? a/b : indeterminado)}");
+/*End Operators*/
+
+
