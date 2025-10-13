bd4b8bcafcbeb673b43a2e967f3777f1ddb417e8
Kelly Andrea Calle <kellycalle317298@correo.itm.edu.co>
2025-10-13 17:16:47 -0500

    Proyecto de clases y herencia creado

---
+++ clases_y_herencia/Program.cs
@@ -0,0 +1,32 @@
+﻿
+/*Clases*/
+//var rect = new Rectangulo ();
+//rect.Base = 20;
+//rect.Altura = 10;
+//Console.WriteLine($"El área es: {rect.Area()}");
+//Console.WriteLine($"El perimetro es: {rect.Perimetro()}");
+/*end Clases*/
+
+var cuad = new Cuadrado();
+cuad.Base = 20;
+cuad.Altura = 10;
+Console.WriteLine($"El área es: {cuad.Area()}");
+Console.WriteLine($"El perimetro es: {cuad.Perimetro()}");
+Console.WriteLine($"El lado es: {cuad.Lado}");
+class Rectangulo
+{
+    public double Base { get; set; }
+    public double Altura { get; set; }
+    public double Area() => Base * Altura;
+    public double Perimetro() => 2 * Base + 2 * Altura;
+}
+
+class Cuadrado : Rectangulo
+{
+    public double Lado
+    {
+        set { Base = value; Altura = value; }
+        get { return Base; }
+    }
+}
+
